174bbf205a25df4d3a73c24449a6d9fd3982ffcc
tekhuy <huyle20@hotmail.com>
2015-01-25 14:49:19 +0000

    another way of creating fizzbuzz in js

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -29,3 +29,16 @@ FizzBuzz.prototype.upTo = function(last_number) {
 	for (var i = 1; i <= last_number; i++) {array.push(this.say(i))};
 	return array;
 };
+
+// Another way:
+// for (i = 1; i <= 20; i++) {
+//     if((i % 3 === 0) && (i % 5 === 0)) {
+//         console.log("FizzBuzz");
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//             } else if (i % 3 === 0) {
+//                 console.log("Fizz");
+//                 } else {
+//                     console.log(i);
+//                     };
+//     };
